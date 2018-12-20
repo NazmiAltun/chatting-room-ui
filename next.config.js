d30d480bd07b7dd60d5ad22198900fb235e49bb8
Nazmi Altun <naa048@gatecoin.com>
2018-12-20 11:39:24 +0800

    Minor lint fix

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ const withTs = require("@zeit/next-typescript");
 module.exports = withTs({
   webpack(config, options) {
     config.module.rules.push({
-      test: /\.svg$/,
       loader: "svg-inline-loader",
+      test: /\.svg$/,
     });
     config.resolve.modules.push(__dirname);
     return config;
